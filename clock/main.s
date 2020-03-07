f15edba123723854fa02890b955ec5ab3a6bd484
Falk Rehwagen <frehwagen@bluewaysw.de>
2020-03-07 20:15:35 +0100

    First fully functional version of RTC clock application added.

--- clock/main.s
+++ clock/main.s
@@ -8,11 +8,6 @@
 
 .segment "STARTUP"
 
-DirEntry:
-		.repeat	64+1+11+4+4+1
-			.byte 0
-			.endrep
-
 DateTimeString:
 		.byte	PLAINTEXT
 TagZehner:	.byte	"0"
@@ -43,99 +38,473 @@ MinZehner:	.byte	"0"
 		.byte	PLAINTEXT
 MinEiner:	.byte	"0"
 		.byte	PLAINTEXT
+		.byte	":"
+		.byte	PLAINTEXT
+SecZehner:	.byte	"0"
+		.byte	PLAINTEXT
+SecEiner:	.byte	"0"
+		.byte	PLAINTEXT
 		.byte	"  "
 		.byte	NULL
 
 __STARTUP_RUN__:
+		lda	firstBoot
+		cmp	#$FF
+		bne	@1
+		
+		; show dialog to operate the mounts
+		LoadW	r0, SelectDialog
+		jsr	DoDlgBox
 
-	lda	firstBoot
-	cmp	#$FF
-	bne	@1
+		jmp	EnterDeskTop		
 
-	; show dialog to operate the mounts
-	LoadW	r0, SelectDialog
-	jsr	DoDlgBox
-@1:
-	jmp	EnterDeskTop
-	
-SelectDialog:
-	.byte	$81	; standard dialog, light bachground
+@1:		jsr	GetRTC
+		bcs	@2	; branch if no valid GetRTC
 
-	.byte	DBTXTSTR
-	.byte	10, 25
-	.word	Text1
-	
-	.byte	OK
-	.byte	16, 70
-	
-	.byte	DB_USR_ROUT
-	.word	DrawDialog
+		jsr	SetTime
+@2:
+		jmp	EnterDeskTop
+		
 
-	.byte	NULL
-	
-Text1:
-	.byte	"Hello World!", NULL
-	
-	
-DrawDialog:
-	lda	#3
-	jsr	SetPattern
-	
-	LoadW	r3, 50
-	LoadW	r4, 100
-	LoadB	r2L, 50
-	LoadB	r2H, 100
-	jsr	Rectangle
-	
-	jsr	LoadImageList
-	
-	jsr	DrawImageList
-	
-	;jsr	OpenDir
-	;jsr	ReadDir
-	
-	jsr	GetDateTimeString
-	
-	;LoadW	r0, DirEntry
-	LoadW	r0, DateTimeString
-	LoadW	r11, 70
-	LoadB	r1H, 100
-	jsr	PutString
-	rts
-	
-LoadImageList:
-	rts
-	
-DrawImageList:
-	rts
-	
-GetDateTimeString:
-		; load RTC base for mega65: ffd7110
-		;LoadW	r0, $7110
-		;LoadW	r1, $0DDF
+GetRTC:
+		; check if clock is running by checking
+		; if seconds are changing for 3s
+		LoadW	r0, $7110
+		LoadW	r1, $0FFD
+				
 		jsr	GetLongUnbounced
 		sta	MySec
-		;inc	r0L
+
+		lda	#30
+		sta	dblClickCount		
+@1:
+		jsr	GetLongUnbounced
+
+		ldx	dblClickCount
+		beq	@2
+		cmp	MySec
+		beq	@1
+
+		; seconds changed, so read the rtc time now
+		inc	r0L
 		jsr	GetLongUnbounced
 		sta	MyMin
 	
-		;inc	r0L
+		inc	r0L
 		jsr	GetLongUnbounced
 		sta	MyStd
 	
-		;inc	r0L
+		and	#$80
+		beq	@10		; branch if 12h time
+		
+		lda	MyStd
+		and	#$3F
+		sta	MyStd
+		bra	@20
+@10:
+		lda	MyStd
+		and	#$20
+		beq	@30
+
+		; pm
+		PushW	r0
+		PushW	r1
+		lda	MyStd
+		and	#$1F
+		jsr	BcdToDec
+		
+		clc
+		adc	#12
+		jsr	DezBCD
+		sta	MyStd
+
+		PopW	r1
+		PopW	r0
+		bra	@20
+
+@30:		; am
+		lda	MyStd
+		and	#$1F
+		sta	MyStd
+@20:
+		inc	r0L
 		jsr	GetLongUnbounced
 		sta	MyTag
 	
-
-		;inc	r0L
+		inc	r0L
 		jsr	GetLongUnbounced
 		sta	MyMonat
 	
-		;inc	r0L
+		inc	r0L
 		jsr	GetLongUnbounced
 		sta	MyJahr
+	
+		clc
+		rts
+@2:
+		sec		; signal error
+		rts
 
+SetRTC:
+		LoadW	r0, $7118
+		LoadW	r1, $0FFD
+		
+		jsr	GetLongUnbounced
+		pha
+		lda	#$41
+		jsr	SetLong
+		jsr	Wait
+
+		LoadW	r0, $7110
+		LoadW	r1, $0FFD
+		
+		; enable setting the RTC
+		
+		lda	MySec	;s
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	MyMin	;m
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+		
+		inc	r0L
+
+		;lda	MyStd	;h
+		jsr	GetLongUnbounced
+		and	#$80
+		beq	@10
+		
+		lda	MyStd
+		ora	#$80
+		bra	@30
+@10:				
+		LoadB	r2L, 0
+		PushW	r0
+		PushW	r1
+		lda	MyStd
+		jsr	BcdToDec
+		cmp	#12
+		blt	@31
+		sec
+		sbc	#12
+		tax
+		LoadB	r2L, $20
+		txa
+@31:
+		jsr	DezBCD
+		tax
+		PopW	r1
+		PopW	r0
+		txa
+		ora	r2L
+@30:
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	MyTag	;d
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	MyMonat	;m
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	MyJahr	;y
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	#$02	;wd
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+
+		lda	#$02	;ids
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+
+		pla
+		inc	r0L
+		jsr	SetLong
+		jsr 	Wait
+		
+		rts
+		
+SetTime:
+		jsr	RunClock
+		rts
+
+GetTime:
+		MoveW_	keyVector,Oldkey
+		LoadW	keyVector,Mykey
+		ldx	#0
+		stx	TabZeiger
+		LoadB	TagZehner-1,REV_ON
+		jsr	ShowClock
+@20:		rts
+
+Oldkey:		.word	0
+
+
+; Mykey
+Mykey:		lda	keyData
+		cmp	Obergrenze
+		bgt	@5
+		cmp	#$30
+		blt	@5
+		pha
+		jsr	SetPlain
+		pla
+		sta	TagZehner,x
+		jmp	@7
+@5:		cmp	#KEY_RIGHT
+		bne	@10
+		jsr	SetPlain
+@7:		iny
+		cpy	#12
+		bne	@15
+		ldy	#0
+@15:		jmp	SetRev
+@10:		cmp	#KEY_DELETE
+		beq	@11
+		cmp	#KEY_LEFT
+		bne	@100
+@11:		jsr	SetPlain
+		dey
+		bpl	@25
+		ldy	#9
+@25:		jmp	SetRev
+@100:		cmp	#CR
+		bne	@1000
+		jsr	SetPlain
+		jsr	GetTimeString
+		jsr	TestTime
+		beq	@120
+		tya
+		pha
+		lda	#2
+		jsr	Beep
+		pla
+		tay
+		jmp	SetRev	
+@120:		jsr	RunClock
+		jsr	ShowClock
+		;jsr	MouseUp
+		;ldx	#1
+		;jsr	UnblockProcess
+		MoveW_	Oldkey,keyVector
+		;LoadB	ModDepth,0
+		jsr	SetRTC
+		jsr	RstrFrmDialogue
+@1000:		rts
+	
+
+SetPlain:
+		ldy	TabZeiger
+		ldx	RevTab,y
+		lda	#PLAINTEXT
+		sta	TagZehner-1,x
+		rts
+
+SetRev:
+		ldx	RevTab,y
+		lda	#REV_ON
+		sta	TagZehner-1,x
+		sty	TabZeiger
+		jsr	SetTime
+		jsr	DoneWithIO
+		jsr	SetObUn
+		jmp	ShowClock
+		
+SetObUn:
+		ldy	TabZeiger
+		lda	ObTab,y
+		sta	Obergrenze
+		rts
+		
+ObTab:		.byte	$33,$39,$31,$39
+		.byte	$39,$39,$32,$39,$35,$39, $35,$39		
+
+; TestTime
+; ]berpr}fung
+; return
+; a = 0 OK
+; a = $ff false; y Wert f}r TabZeiger
+		
+TestTime:
+		lda	MyTag
+		jsr	BcdToDec
+		cmp	#0
+		bne	@10
+		ldy	#1
+@00:
+		lda	#$ff
+		rts
+@10:		cmp	#32
+		blt	@05
+		ldy	#0
+		beq	@00
+		
+@05:		lda	MyMonat
+		jsr	BcdToDec
+		cmp	#0
+		bne	@20
+@31:		ldy	#3
+		bne	@00
+@20:		cmp	#13
+		blt	@30
+		ldy	#2
+		bne	@00
+@30:		cmp	#2	; Feb
+		bne	@40
+		lda	MyTag
+		jsr	BcdToDec
+		cmp	#29
+		beq	@35
+		bgt	@31
+		jmp	@th
+@35:		lda	MyJahr
+		jsr	BcdToDec
+		ror		; /2
+		bcc	@32
+@33:		ldy	#1
+		jmp	@00
+@32:		ror		; /4
+		bcs	@33
+		jmp	@th
+@40:		cmp	#8
+		bge	@45
+		ror
+		bcc	@42
+@43:		jmp	@th	; ungerade
+@42:		lda	MyTag
+		jsr	BcdToDec
+		cmp	#31
+		bne	@43
+		ldy	#1
+		jmp	@00
+@45:		ror
+		bcs	@42
+@th:		clc
+		lda	MyStd
+		jsr	BcdToDec
+		cmp	#24
+		bge	@th1
+		lda	#$00
+		rts
+@th1:		ldy	#6
+		jmp	@00
+
+; Beep
+; a - Anzahl Beep
+Beep:		sta	@AnzBeep
+		lda 	#$0f
+		jsr	InitForIO
+		sta 	$d418
+		lda 	#$00
+		sta 	$d405
+		lda 	#$f7
+		sta 	$d406
+		lda 	#$11
+		sta 	$d404
+		lda 	#$32
+		sta 	$d401
+		lda 	#$00
+		sta 	$d400
+@10:		lda	#$0f
+		sta	$d418
+		jsr	@Wait
+		lda 	#$00
+		sta 	$d418
+		jsr	@Wait
+		dec	@AnzBeep
+		bne	@10
+		lda 	#$10
+		sta 	$d404
+		lda 	#$00
+		sta 	$d418
+		jsr	DoneWithIO
+		rts
+@Wait:
+		jsr	DoneWithIO
+		ldx	#3
+		stx	dblClickCount
+@20:
+		ldx	dblClickCount
+		bne	@20
+		jsr	InitForIO
+		rts
+@AnzBeep:	.byte	2
+
+TabZeiger:
+		.byte	0
+RevTab:
+		.byte	0,2,6,8,12,14
+		.byte	19,21,25,27,31,33
+
+Obergrenze:
+		.byte	"3"
+						
+SelectDialog:
+		.byte	$81	; standard dialog, light bachground
+
+		.byte	DBTXTSTR
+		.byte	10, 25
+		.word	Text1
+		
+		.byte	CANCEL
+		.byte	17, 70
+
+		.byte	DB_USR_ROUT
+		.word	DrawDialog
+
+		.byte	NULL
+	
+Text1:
+		.byte	BOLDON, "Set RTC and system date/time:", NULL
+	
+	
+DrawDialog:
+		jsr	GetDateTimeString
+		jsr	GetTime
+	
+ShowClock:
+		LoadW	r0, DateTimeString
+		LoadW	r11, 80
+		LoadB	r1H, 85
+		jsr	PutString
+		rts
+	
+	
+GetDateTimeString:
+		jsr	GetRTC
+		bcc	@2
+		
+		; init from GEOS time
+		lda	day
+		jsr	DezBCD
+		sta	MyTag
+		lda	month
+		jsr	DezBCD
+		sta	MyMonat
+		lda	year
+		jsr	DezBCD
+		sta	MyJahr
+		lda	hour
+		jsr	DezBCD
+		sta	MyStd
+		lda	minutes
+		jsr	DezBCD
+		sta	MyMin
+		lda	seconds
+		jsr	DezBCD
+		sta	MySec
+@2:
 		ldx	MyTag
 		lda	#TagZehner-TagZehner
 		jsr	DivnSet
@@ -151,19 +520,52 @@ GetDateTimeString:
 		ldx	MyMin
 		lda	#MinZehner-TagZehner
 		jsr	DivnSet
+		ldx	MySec
+		lda	#SecZehner-TagZehner
+		jsr	DivnSet
+
+		rts
+
+Wait:
+		LoadW	r2, $71FF
+		LoadW	r3, $0FFD
+		
+@10:
+		LDZ	#0
+		EOM
+		lda 	(r2), Z
+		bne	@10
+		
 		rts
 
 GetLongUnbounced:
-		lda	#1
+		LDZ	#0
+		EOM
+		lda 	(r0), Z
+		EOM
+	 	cmp	(r0), Z
+		bne 	GetLongUnbounced
+		EOM
+	 	cmp	(r0), Z
+		bne 	GetLongUnbounced
 		rts
-
+		
+SetLong:
+		LDZ	#0
+		EOM
+		sta 	(r0), Z
+		RTS
+		
 DivnSet:	pha
-		stx	r0L
-Div10:		LoadB	r0H,0
-		ldx	#r0L
-		LoadW___	r1,10
-		ldy	#r1L
-		jsr	Ddiv
+		txa
+		lsr
+		lsr
+		lsr
+		lsr
+		sta	r0L
+		txa
+		and	#$0F
+		sta	r8L
 		pla
 		tax
 		lda	r0L
@@ -183,100 +585,147 @@ MyTag:		.byte	0
 MyStd:		.byte	0
 MyMin:		.byte	0
 MySec:		.byte 	0	
-ImageList:
-	.repeat	100
-		.byte 0
-	.endrep
-	
-	OpenDir:
-		;; Opendir takes no arguments and returns File descriptor in A
-		LDA #$12
-		STA $D640
-		NOP
-		LDX #$00
-		RTS
-		
 
-		;; readdir takes the file descriptor returned by opendir as argument
-		;; and gets a pointer to a MEGA65 DOS dirent structure.
-		;; Again, the annoyance of the MEGA65 Hypervisor requiring a page aligned
-		;; transfer area is a nuisance here. We will use $0400-$04FF, and then
-		;; copy the result into a regular C dirent structure
-		;;
-		;; d_ino = first cluster of file
-		;; d_off = offset of directory entry in cluster
-		;; d_reclen = size of the dirent on disk (32 bytes)
-		;; d_type = file/directory type
-		;; d_name = name of file
-	ReadDir:
-	.if 0
+
+DPA	= $dc00
+ampm:		.byte	0
+
+GetTimeString:
+		lda	TagZehner
+		ldx	TagEiner
+		jsr	ASCBCD
+		sta	MyTag
+		
+		lda	MonZehner
+		ldx	MonEiner
+		jsr	ASCBCD
+		sta	MyMonat
+		
+		lda	JahZehner
+		ldx	JahEiner
+		jsr	ASCBCD
+		sta	MyJahr
+		
+		lda	StdZehner
+		ldx	StdEiner
+		jsr	ASCBCD
+		sta	MyStd
+
+		lda	MinZehner
+		ldx	MinEiner
+		jsr	ASCBCD
+		sta	MyMin
+		
+		lda	SecZehner
+		ldx	SecEiner
+		jsr	ASCBCD
+		sta	MySec
+
+		rts
+
+SetTime2:
+		jsr	InitForIO
+		
+		lda	MyTag
+		jsr	BcdToDec
+		sta	day
+		
+		lda	MyMonat
+		jsr	BcdToDec
+		sta	month
+
+		lda	MyJahr
+		jsr	BcdToDec
+		sta	year
+		
+		LoadB	ampm,0
+		lda	MyStd
+		jsr	BcdToDec
+		
+		cmp	#24	; accept 24 to be 0 o'clock
+		bne	@ci
+		lda	#0
+@ci:		sta	hour
+
+		cmp	#12
+		blt	@am	
 		pha
+		LoadB	ampm,$80
+		pla
+		sec
+		sbc	#12
 		
-		;; First, clear out the dirent
-		ldx #0
+@am:		jsr	DezBCD
+		clc
+		adc	ampm
+		sta	DPA+$0b
+
+		lda	MyMin
+		jsr	BcdToDec		
+		sta	minutes
+		jsr	DezBCD
+		sta	DPA+$0a
+		
+		lda	MySec
+		jsr	BcdToDec		
+		sta	seconds
+		jsr	DezBCD
+		sta	DPA+$09
+		
+		rts
+		
+RunClock:
+		jsr	SetTime2
+		lda	#$00
+		sta	DPA+$08
+		jsr	DoneWithIO
+		rts	
+;ASCBCD
+; a High
+; x Low
+; Return a
+
+ASCBCD:
+		asl
+		asl
+		asl
+		asl
+		sta	r0L
 		txa
-	@l1:	sta @dirent,x	
-		dex
-		bne @l1
-
-		;; Third, call the hypervisor trap
-		;; File descriptor gets passed in in X.
-		;; Result gets written to transfer area we setup at $0400
-
-
-		plx
-	.endif
-		tax
-		ldy #>DirEntry 		; write dirent to DirEntry 
-
-		lda #$14
-		STA $D640
-		NOP
-
-		bcs @readDirSuccess
-
-		;;  Return end of directory
-		lda #$00
-		ldx #$00
-		RTS
-
-	@readDirSuccess:
-	.if 0	
-		;;  Copy file name
-		ldx #$3f
-	@l2:	lda $0400,x
-		sta _readdir_dirent+4+2+4+2,x
-		dex
-		bpl @l2
-		;; make sure it is null terminated
-		ldx $0400+64
-		lda #$00
-		sta _readdir_dirent+4+2+4+2,x
-
-		;; Inode = cluster from offset 64+1+12 = 77
-		ldx #$03
-	@l3:	lda $0477,x
-		sta _readdir_dirent+0,x
-		dex
-		bpl @l3
-
-		;; d_off stays zero as it is not meaningful here
+		and	#$0f
+		ora	r0L
+		rts
+	
+DezBCD:
+		sta	r0L
+		LoadB	r0H,0
+		ldx	#r0L
+		LoadW	r1,10
+		ldy	#r1L
+		jsr	Ddiv
+		lda	r0L
+		asl
+		asl
+		asl
+		asl
+		clc
+		adc	r8L
+		rts
 		
-		;; d_reclen we preload with the length of the file (this saves calling stat() on the MEGA65)
-		ldx #3
-	@l4:	lda $0400+64+1+12+4,x
-		sta _readdir_dirent+4+2,x
-		dex
-		bpl @l4
+BcdToDec:
+		tax
+		and	#$0F
+		sta	r0L
+		txa
+		and	#$F0
+		lsr
+		; by 8 plus by 2
+		sta	r0H
+		lsr
+		lsr
+		clc
+		adc	r0H
+		clc
+		adc	r0L
 
-		;; File type and attributes
-		;; XXX - We should translate these to C style meanings
-		lda $0400+64+1+12+4+4
-		sta _readdir_dirent+4+2+4
-
-		;; Return address of dirent structure
-		lda #<_readdir_dirent
-		ldx #>_readdir_dirent
-	.endif
-		RTS
-
+		rts
